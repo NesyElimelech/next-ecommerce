509603003f836e32367c0efe516388ad5970527b
NesyElimelech <nesy15@gmail.com>
2023-08-24 17:50:45 +0300

    add stripe webhooks to update the order status on the DB

--- pages/api/webhooks.ts
+++ pages/api/webhooks.ts
@@ -1,14 +1,55 @@
 import Stripe from "stripe";
 import { PrismaClient } from "@prisma/client";
-import { buffer } from 'micro';
-import { NextApiRequest, NextApiResponse } from 'next';
+import { buffer } from "micro";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
   apiVersion: "2022-11-15",
 });
 const prisma = new PrismaClient();
 
+export default async function handlers(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const buf = await buffer(req);
+  const sig = req.headers["stripe-signature"];
 
-export default async function handlers(req: NextApiRequest, res: NextApiResponse) {
-  
+  if (!sig) return res.status(400).send("Missing the stripe signature");
+
+  let event: Stripe.Event;
+  try {
+    event = stripe.webhooks.constructEvent(
+      buf,
+      sig,
+      process.env.STRIPE_WEBHOOK_SECRET!
+    );
+  } catch (error) {
+    return res.status(400).send("Webhook error: " + error);
+  }
+  /* Handle different types of events */
+
+  switch (event.type) {
+    case "payment_intent.created":
+      const paymentIntent = event.data.object;
+      console.log("Payment intent was created");
+      break;
+    case "charge.succeeded":
+      const charge = event.data.object as Stripe.Charge;
+      if (typeof charge.payment_intent === "string") {
+        const order = await prisma.order.update({
+          where: { paymentIntentID: charge.payment_intent },
+          data: { status: "complete" },
+        });
+      }
+      break;
+    default:
+      break;
+  }
 }
